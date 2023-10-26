1936f04a13d65be23a91f89b2d86a3e428253abd
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-10-26 20:41:55 +0300

    cleanup

--- Server/server.mjs
+++ Server/server.mjs
@@ -32,42 +32,7 @@ app.use(bodyParser.json());
 
 app.use('/api/user', userRoutes);
 
-// app.post("/api/login", catchAsync(async (req, res) => {
-//   const { username, password } = req.body;
 
-//   console.log(username, password);
-
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       username: username
-//     }
-//   })
-
-//   console.log(user);
-//   const compare = await bcrypt.compare(password, user.password);
-//   if (user && compare) {
-//     console.log('Same')
-//     res.status(200).json({ authenticated: true, id: user.id, message: "Authentication successful" });
-//   } else {
-//     res.status(401).json({ authenticated: false, message: "Authentication failed" });
-//   }
-// }));
-
-// app.post("/api/signup", catchAsync(async (req, res) => {
-//   const { email, username, password } = req.body;
-//   const salt = await bcrypt.genSalt(10);
-//   const hash = await bcrypt.hash(password, salt);
-//   const user = await prisma.user.create({
-//     data: {
-//       email: email,
-//       username: username,
-//       password: hash
-//     }
-//   })
-//   console.log(user);
-
-//   res.status(201).json({ message: "Registration successful" });
-// }));
 
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
