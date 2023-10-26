16ea262c3b011c90dcb13122c5c7b3eb38bc6ab8
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-10-26 23:08:59 +0300

    post route

--- Server/routes/TicketRoutes.mjs
+++ Server/routes/TicketRoutes.mjs
@@ -6,17 +6,14 @@ import config from "../firebase.mjs";
 import { initializeApp } from "firebase/app"
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
 const router = express.Router();
+const prisma = new PrismaClient();
 
 initializeApp(config);
-
-
 const storage = getStorage();
-
-
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/", upload.single("file"), catchAsync(async (req, res) => {
-
+    const { userId, title, text } = req.body;
     const storageRef = ref(storage, `tickets/${req.file.originalname}`);
     const metadata = {
         contentType: req.file.mimetype
@@ -24,6 +21,30 @@ router.post("/", upload.single("file"), catchAsync(async (req, res) => {
 
     const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
     const downloadURL = await getDownloadURL(snapshot.ref);
+
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         id: userId
+    //     }
+    // })
+    // console.log(user);
+    // const ticket = await prisma.ticket.create({
+    //     data:
+    //     {
+    //         title: title,
+    //         text: text,
+    //         authorId: user.id,
+    //         file: {
+    //             create: {
+    //                 file_name: req.file.originalname,
+    //                 url: downloadURL
+    //             }
+    //         },
+    //     },
+
+
+    // });
+    console.log(ticket);
     console.log('File successfully uploaded.', downloadURL);
     res.send('Uploaded');
 }));
