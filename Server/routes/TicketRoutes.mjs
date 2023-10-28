9a3701b63c346e49d499d0ccb20d7f808cdb588d
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-10-28 17:10:06 +0300

    ticket routes

--- Server/routes/TicketRoutes.mjs
+++ Server/routes/TicketRoutes.mjs
@@ -12,41 +12,62 @@ initializeApp(config);
 const storage = getStorage();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/", upload.single("file"), catchAsync(async (req, res) => {
-    const { userId, title, text } = req.body;
-    const storageRef = ref(storage, `tickets/${req.file.originalname}`);
-    const metadata = {
-        contentType: req.file.mimetype
-    }
+router.route("/")
+    .post(upload.single("file"), catchAsync(async (req, res) => {
+        const { userId, title, text } = req.body;
+        const storageRef = ref(storage, `tickets/${req.file.originalname}`);
+        const metadata = {
+            contentType: req.file.mimetype
+        }
 
-    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
-    const downloadURL = await getDownloadURL(snapshot.ref);
+        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+        const downloadURL = await getDownloadURL(snapshot.ref);
 
-    // const user = await prisma.user.findUnique({
-    //     where: {
-    //         id: userId
-    //     }
-    // })
-    // console.log(user);
-    // const ticket = await prisma.ticket.create({
-    //     data:
-    //     {
-    //         title: title,
-    //         text: text,
-    //         authorId: user.id,
-    //         file: {
-    //             create: {
-    //                 file_name: req.file.originalname,
-    //                 url: downloadURL
-    //             }
-    //         },
-    //     },
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        console.log(user);
+        const ticket = await prisma.ticket.create({
+            data:
+            {
+                title: title,
+                text: text,
+                authorId: user.id,
+                file: {
+                    create: {
+                        file_name: req.file.originalname,
+                        url: downloadURL
+                    }
+                },
+            },
+            include: {
+                file: true
+            }
 
-
-    // });
-    console.log(ticket);
-    console.log('File successfully uploaded.', downloadURL);
-    res.send('Uploaded');
-}));
+        });
+        console.log(ticket);
+        console.log('File successfully uploaded.', downloadURL);
+        res.status(200).json({ posted: true });
+    }))
+    .get(catchAsync(async (req, res) => {
+        const { userId } = req.body;
+        console.log(userId);
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            },
+            include: {
+                tickets: {
+                    include: {
+                        file: true
+                    }
+                }
+            }
+        })
+        res.status(200);
+        res.json(user.tickets);
+    }))
 
 export default router;
